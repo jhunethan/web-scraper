6bcad8b85b0aaec0a90cf0846833d50afd807988
jhunethan <jhunethan2812@gmail.com>
2021-09-07 18:33:20 +0100

    exports data as excel files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const Excel = require("exceljs");
 
 async function getRestaurants() {
   const browser = await puppeteer.launch();
@@ -10,15 +11,35 @@ async function getRestaurants() {
     return Array.from(document.querySelectorAll(".c-listing-item-title")).map(
       (el) => {
         const formattedContent = el.textContent.split("\n")[1].trim();
-
         return formattedContent;
       }
     );
   });
 
-  console.log(restuarants);
-  await fs.promises.writeFile("restaurants.json", JSON.stringify({restuarants}));
+  console.log(restuarants)
+  await writeExcel(restuarants);
   await browser.close();
+}
+
+async function writeExcel(data) {
+  const workbook = new Excel.Workbook();
+  const worksheet = workbook.addWorksheet("My Sheet");
+
+  worksheet.columns = [
+    { header: "Id", key: "id", width: 10 },
+    { header: "Name", key: "name", width: 32 },
+  ];
+
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    worksheet.addRow({ id: i, name: data[i] });
+  }
+
+  // save under export.xlsx
+  await workbook.xlsx.writeFile("export.xlsx");
+
+  console.log("File is written");
 }
 
 getRestaurants();

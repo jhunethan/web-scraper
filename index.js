0240f9b8b19bb16bb40ff77ef66be62d9616904d
jhunethan <jhunethan2812@gmail.com>
2021-09-09 15:31:33 +0100

    scrapes menu items from a single restaurant

--- index.js
+++ index.js
@@ -2,38 +2,46 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const Excel = require("exceljs");
 
-async function getRestaurants() {
+async function getRestaurants(uri) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.just-eat.co.uk/area/bs1-bristol");
+  await page.goto(uri);
 
   const restuarants = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".c-listing-item-title")).map(
+    return Array.from(document.querySelectorAll(".c-menuItems-content")).map(
       (el) => {
-        const formattedContent = el.textContent.split("\n")[1].trim();
-        return formattedContent;
+        const item = {};
+        item.name = el.children[0].textContent.split("\n")[2].trim();
+        item.price = +el.textContent.split("£")[1].split("\n")[0].trim();
+        if (el.children[2])
+          item.description = el.children[1].textContent.split("\n")[1].trim();
+
+        return item;
       }
     );
   });
 
-  console.log(restuarants)
-  await writeExcel(restuarants);
+  const restaurantName = uri.split("/")[3];
+  await writeExcel(restuarants, restaurantName);
   await browser.close();
 }
 
-async function writeExcel(data) {
+async function writeExcel(items, restaurantName) {
   const workbook = new Excel.Workbook();
   const worksheet = workbook.addWorksheet("My Sheet");
 
   worksheet.columns = [
     { header: "Id", key: "id", width: 10 },
-    { header: "Name", key: "name", width: 32 },
+    { header: "Restaurant", key: "restaurant", width: 32 },
+    { header: "Name", key: "name", width: 40 },
+    { header: "Price", key: "price", width: 10 },
+    { header: "Description", key: "description", width: 100 },
   ];
 
-  console.log(data);
+  console.log(items);
 
-  for (let i = 0; i < data.length; i++) {
-    worksheet.addRow({ id: i, name: data[i] });
+  for (let i = 0; i < items.length; i++) {
+    worksheet.addRow({ id: i, restaurant: restaurantName, ...items[i] });
   }
 
   // save under export.xlsx
@@ -42,4 +50,6 @@ async function writeExcel(data) {
   console.log("File is written");
 }
 
-getRestaurants();
+getRestaurants(
+  "https://www.just-eat.co.uk/restaurants-subway-filtonbristol/menu"
+);
